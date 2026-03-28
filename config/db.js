8a53848ed83c4a08310f8876b1ecdd7ba32b3ba8
sreenidhi31 <sreeenidhiii10@gmail.com>
2026-03-28 18:36:28 +0530

    fixed

--- config/db.js
+++ config/db.js
@@ -1,18 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+// Use connection pool instead of single connection
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error("DB Connection Failed:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
